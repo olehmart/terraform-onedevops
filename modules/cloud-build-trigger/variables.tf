412d3b3feb48ee300c5165ea90437e6217a13847
Oleh Martushevskyi <olehmart7@gmail.com>
2022-04-04 18:48:21 -0400

    GCP Cloud build trigger module creation

---
+++ modules/cloud-build-trigger/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  type = string
+  description = "GCP project ID"
+}
+variable "config_path" {
+  type = string
+  description = "YAML config path for GCP cloud build triggers configuration"
+}
